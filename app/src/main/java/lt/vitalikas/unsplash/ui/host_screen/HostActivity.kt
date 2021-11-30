bb5de81e2e12e751a7e4d0ae8ab52d8cdad391bd
Vitalijus <vitalijuskolinko@gmail.com>
2021-11-30 12:26:08 +0200

    dependencies added

---
+++ app/src/main/java/lt/vitalikas/unsplash/ui/host_screen/HostActivity.kt
@@ -0,0 +1,6 @@
+package lt.vitalikas.unsplash.ui.host_screen
+
+import androidx.appcompat.app.AppCompatActivity
+import lt.vitalikas.unsplash.R
+
+class HostActivity : AppCompatActivity(R.layout.activity_host)
