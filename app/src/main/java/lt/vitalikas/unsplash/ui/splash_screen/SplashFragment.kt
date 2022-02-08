55011e2d43bda6b3c53b1a751ff2074500624d64
Vitalijus <vitalijuskolinko@gmail.com>
2022-02-08 19:06:19 +0200

    optimizing logic

--- app/src/main/java/lt/vitalikas/unsplash/ui/splash_screen/SplashFragment.kt
+++ app/src/main/java/lt/vitalikas/unsplash/ui/splash_screen/SplashFragment.kt
@@ -1,72 +1,75 @@
-package lt.vitalikas.unsplash.ui.start_screen
+package lt.vitalikas.unsplash.ui.splash_screen
 
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import lt.vitalikas.unsplash.R
 import lt.vitalikas.unsplash.databinding.FragmentStartBinding
 
 @AndroidEntryPoint
-class StartFragment : Fragment(R.layout.fragment_start) {
+class SplashFragment : Fragment(R.layout.fragment_start) {
 
     private val binding by viewBinding(FragmentStartBinding::bind)
     private val startImage get() = binding.ivStartImage
     private val startTitle get() = binding.tvStartTitle
     private val loadingText get() = binding.tvLoadingText
 
-    private val startViewModel by viewModels<StartViewModel>()
+    private val splashViewModel by viewModels<SplashViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        bindViewModel()
-        lifecycleScope.launch {
-            delay(500)
-            bindData()
-            start()
-        }
+        bindData()
     }
 
     private fun bindData() {
         startTitle.text = getString(R.string.start_text)
-    }
 
-    private fun bindViewModel() {
-        startViewModel.step.observe(viewLifecycleOwner) { millisLeft ->
-            loadingText.text = (millisLeft / (1000 - 100)).toString()
-            loadImage(((millisLeft / (1000 - 100))).toInt())
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                delay(500)
+
+                launch {
+                    splashViewModel.timerFlow
+                        .collect { step ->
+                            if (step == 0) {
+                                val onBoardingStatus = splashViewModel.onboardingNotFinished
+                                navigateToNextScreen(onBoardingStatus)
+                            } else {
+                                loadingText.text = step.toString()
+                                loadImageByStep(step)
+                            }
+                        }
+                }
+            }
         }
-
-        startViewModel.onboardingNotFinishedStatus.observe(viewLifecycleOwner) { onboardingNotFinished ->
-            navigateToNextScreen(onboardingNotFinished)
-        }
-    }
-
-    private fun start() {
-        startViewModel.timer?.start()
     }
 
     private fun navigateToNextScreen(onboardingNotFinished: Boolean) {
         if (onboardingNotFinished) {
             findNavController().navigate(
-                StartFragmentDirections.actionStartFragmentToOnboardingFragment()
+                SplashFragmentDirections.actionStartFragmentToOnboardingFragment()
             )
         } else {
             findNavController().navigate(
-                StartFragmentDirections.actionStartFragmentToAuthFragment()
+                SplashFragmentDirections.actionStartFragmentToAuthFragment()
             )
         }
     }
 
-    private fun loadImage(num: Int) {
-        val imageUri = "@drawable/start$num"
+    private fun loadImageByStep(step: Int) {
+        val imageUri = "@drawable/start$step"
         val imageRes = resources.getIdentifier(imageUri, "drawable", context?.packageName)
         Glide.with(this)
             .load(imageRes)
--- app/src/main/java/lt/vitalikas/unsplash/ui/splash_screen/SplashViewModel.kt
+++ app/src/main/java/lt/vitalikas/unsplash/ui/splash_screen/SplashViewModel.kt
@@ -1,4 +1,4 @@
-package lt.vitalikas.unsplash.ui.start_screen
+package lt.vitalikas.unsplash.ui.splash_screen
 
 import android.content.Context
 import android.os.CountDownTimer
@@ -6,16 +6,23 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import lt.vitalikas.unsplash.R
 import lt.vitalikas.unsplash.domain.repositories.OnboardingRepository
+import lt.vitalikas.unsplash.ui.feed_screen.FeedState
 import javax.inject.Inject
 import kotlin.properties.Delegates
 
 @HiltViewModel
-class StartViewModel @Inject constructor(
+class SplashViewModel @Inject constructor(
     repository: OnboardingRepository,
     @ApplicationContext context: Context
 ) : ViewModel() {
@@ -25,6 +32,12 @@ class StartViewModel @Inject constructor(
     var onboardingNotFinished by Delegates.notNull<Boolean>()
 
     var timer: CountDownTimer? = null
+
+    val timerFlow = (3 downTo 0)
+        .asFlow()
+        .onEach {
+            delay(1000L)
+        }
 
     private val _step = MutableLiveData<Long>()
     val step: LiveData<Long> get() = _step
@@ -40,19 +53,19 @@ class StartViewModel @Inject constructor(
             )
         }
 
-        timer = object : CountDownTimer(3000L, 1000L) {
-            override fun onTick(p0: Long) {
-                _step.postValue(p0)
-            }
-
-            override fun onFinish() {
-                _onboardingNotFinishedStatus.postValue(onboardingNotFinished)
-            }
-        }
+//        timer = object : CountDownTimer(3000L, 1000L) {
+//            override fun onTick(p0: Long) {
+//                _step.postValue(p0)
+//            }
+//
+//            override fun onFinish() {
+//                _onboardingNotFinishedStatus.postValue(onboardingNotFinished)
+//            }
+//        }
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        timer = null
-    }
+//    override fun onCleared() {
+//        super.onCleared()
+//        timer = null
+//    }
 }
